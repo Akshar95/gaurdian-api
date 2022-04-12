a4e448721d4cbb6c6845bb17a097be06781f6fdf
Akshar95 <patel.akshar295@gmail.com>
2022-04-12 21:54:11 +0100

    forEach method

--- index.js
+++ index.js
@@ -5,23 +5,41 @@ const cheerio = require("cheerio");
 
 const app = express();
 
-const newspapers =[
-    {
-        name: 'thetimes',
-        website: "https://www.thetimes.co.uk/environment/climate-change"
-    },
-    {
-        name: 'gaurdian',
-        website: "https://www.theguardian.com/environment/climate-crisis"
-    },
-    {
-        name: 'telegraph',
-        website: "https://www.telegraph.co.uk/climate-change"
-    },
-]
+const newspapers = [
+  {
+    name: "thetimes",
+    website: "https://www.thetimes.co.uk/environment/climate-change",
+  },
+  {
+    name: "gaurdian",
+    website: "https://www.theguardian.com/environment/climate-crisis",
+  },
+  {
+    name: "telegraph",
+    website: "https://www.telegraph.co.uk/climate-change",
+  },
+];
+app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
 
 const articles = [];
-app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.website).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("html");
+
+      articles.push({
+        title,
+        url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
 
 //scrape 1st webpage
 app.get("/", (req, res) => {
@@ -30,22 +48,5 @@ app.get("/", (req, res) => {
 
 //grab the gaurdian url
 app.get("/news", (req, res) => {
-  axios
-    .get("https://www.theguardian.com/environment/climate-crisis")
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('a:contains("climate")', html).each(function () {
-        const title = $(this).text();
-        const url = $(this).attr("href");
-
-        articles.push({
-          title,
-          url,
-        });
-      });
-      res.json(articles);
-    })
-    .catch((err) => console.log(err));
+  res.json(articles);
 });
